8e3ed9bccfba7d7f789c1dc40c565caaf8ac316c
VitorDeFranca <francavit.r@gmail.com>
2026-07-07 18:50:37 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -5,11 +5,35 @@ class Program
         while (true)
         {
             Console.Write("$ ");
+
             var userInput = Console.ReadLine();
-            if (string.Equals(userInput, "exit")) 
-                break;
-            Console.WriteLine($"{userInput}: command not found");
-            
+            if (string.IsNullOrEmpty(userInput) || string.IsNullOrWhiteSpace(userInput))
+                continue;
+
+            var command = userInput.Split(' ')[0];
+            var arguments = userInput.Split(' ').Skip(1).ToList();
+
+            if (string.Equals(command, "exit")) break;
+
+            switch (command)
+            {
+                case "echo":
+                    EchoCommandHandler(arguments);
+                    break;
+                default:
+                    NotAValidCommandHandler(command);
+                    break;
+            };
         }
+    }
+
+    private static void NotAValidCommandHandler(string command)
+    {
+        Console.WriteLine($"{command}: command not found");
+    }
+
+    private static void EchoCommandHandler(List<string> arguments)
+    {
+        Console.WriteLine(String.Join(" ", arguments));
     }
 }
