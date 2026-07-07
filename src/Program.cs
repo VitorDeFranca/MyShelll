f2caa267dacf3805784c2d99bf1e042656f979b7
VitorDeFranca <francavit.r@gmail.com>
2026-07-07 19:18:52 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,12 @@
+using System.Diagnostics.Tracing;
+using System.Text;
+
 class Program
 {
+    private static IEnumerable<string> ShellBuiltIns = new List<string> { "echo", "exit", "type" };
     static void Main()
     {
+        
         while (true)
         {
             Console.Write("$ ");
@@ -11,7 +16,7 @@ class Program
                 continue;
 
             var command = userInput.Split(' ')[0];
-            var arguments = userInput.Split(' ').Skip(1).ToList();
+            var arguments = userInput.Split(' ').Skip(1);
 
             if (string.Equals(command, "exit")) break;
 
@@ -20,6 +25,11 @@ class Program
                 case "echo":
                     EchoCommandHandler(arguments);
                     break;
+
+                case "type":
+                    TypeCommandHandler(arguments);
+                    break;
+
                 default:
                     NotAValidCommandHandler(command);
                     break;
@@ -27,13 +37,42 @@ class Program
         }
     }
 
+    private static void TypeCommandHandler(IEnumerable<string> arguments)
+    {
+        if (arguments.Count() != 1)
+        {
+            Console.WriteLine($"{GetArgumentsString(arguments)} is not a valid argument for 'type'");
+            return;
+        }
+
+        var word = arguments.First();
+        if (ShellBuiltIns.Contains(word))
+        {
+            Console.WriteLine($"{word} is a shell builtin");
+            return;
+        }
+
+        Console.WriteLine($"{word}: not found");
+    }
+
     private static void NotAValidCommandHandler(string command)
     {
         Console.WriteLine($"{command}: command not found");
     }
 
-    private static void EchoCommandHandler(List<string> arguments)
+    private static void EchoCommandHandler(IEnumerable<string> arguments)
     {
-        Console.WriteLine(String.Join(" ", arguments));
+        Console.WriteLine(GetArgumentsString(arguments));
+    }
+
+    private static string GetArgumentsString(IEnumerable<string> arguments) 
+    {
+        var argumentsStringBuilder = new StringBuilder();
+        foreach (var argument in arguments)
+        {
+            argumentsStringBuilder.AppendJoin(" ", argument);
+        }
+
+        return argumentsStringBuilder.ToString();
     }
 }
