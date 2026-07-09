cae379b0859bc54031db9df68fbbd5eea616c477
VitorDeFranca <francavit.r@gmail.com>
2026-07-08 21:45:06 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -84,6 +84,7 @@ class Program
         if (!string.IsNullOrEmpty(filePath) && IsAnExecutable(command))
         {
             Process.Start(filePath, arguments);
+            return;
         }
 
         NotAValidCommandHandler(command);
