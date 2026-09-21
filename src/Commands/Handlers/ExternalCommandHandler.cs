ebdec3f061001bac34efa28f9ccb1cc71a85488a
VitorDeFranca <francavit.r@gmail.com>
2026-09-21 16:22:08 -0300

    codecrafters submit [skip ci]

--- src/Commands/Handlers/ExternalCommandHandler.cs
+++ src/Commands/Handlers/ExternalCommandHandler.cs
@@ -39,8 +39,10 @@ namespace CodeCrafters.Shell.src.Commands.Handlers
 
             var output = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
-
-            return new CommandResult(CommandResultType.Success, TrimTrailingNewLine(output));
+            Console.WriteLine(output);
+            var teste = TrimTrailingNewLine(output);
+            Console.WriteLine(teste);
+            return new CommandResult(CommandResultType.Success, teste);
         }
 
         private static string TrimTrailingNewLine(string output)
